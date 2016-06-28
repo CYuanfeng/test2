98f0a30cac677102950b82fe41dc928358c29ca0
yuan <562033624@qq.com>
2016-06-28 18:33:36 +0800

    设置admin
    admin

---
+++ Restaurant_system/Restaurant_system/AdminItem/normalItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Restaurant_system.Common;
+namespace Restaurant_system.AdminItem
+{
+    class normalItem: Button
+    {
+        public normalItem(string title)
+        {
+            this.Content = title;
+            this.Name = title;
+            this.Background = AppconfigSet.blueColor;
+            this.Foreground = AppconfigSet.whiteColor;
+            this.FontSize = 30;
+            this.Height = 100;
+            this.Margin = new System.Windows.Thickness(10,10,10,0);
+        }
+    }
+}
--- Restaurant_system/Restaurant_system/AdminMain.xaml.cs
+++ Restaurant_system/Restaurant_system/AdminMain.xaml.cs
@@ -23,12 +23,27 @@ namespace Restaurant_system
         public AdminMain()
         {
             InitializeComponent();
-            for (int i = 0; i < 5;i++ )
-            {
-                topItemButton item = new topItemButton("test"+i.ToString());
-                topPanel.Children.Add(item);
-            }
+            topItemButton item1 = new topItemButton("添加快餐");
+            topPanel.Children.Add(item1);
+            item1.Click += fastFoodClick;
+            meatTitle.Visibility = Visibility.Hidden;
+            vegeTitle.Visibility = Visibility.Hidden;
 
+            
+
+           
+
+        }
+        public void fastFoodClick(object obj,EventArgs e)
+        {
+            meatTitle.Visibility = Visibility.Visible;
+            vegeTitle.Visibility = Visibility.Visible;
+            topItemButton item2 = new topItemButton("饭");
+            staplePanel.Children.Add(item2);
+
+
+            normalItem nor1 = new normalItem("菜心");
+            vagePanel.Children.Add(nor1);
         }
     }
 }
